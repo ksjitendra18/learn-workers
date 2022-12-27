5935cbfd9c334962e22323325be99f53edc38448
Jitendra <jitendraks1803@gmail.com>
2022-12-27 17:21:19 +0530

    final

---
+++ pages/api/hello.js
@@ -0,0 +1,12 @@
+export const config = {
+  runtime: "experimental-edge",
+};
+
+export default async function (req) {
+  return new Response(JSON.stringify({ name: "JS" }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
